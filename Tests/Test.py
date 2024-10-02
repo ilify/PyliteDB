43c9f7774d134643af5444f8e01d5d2d52dae40c
ilies <iliesmraihia@gmail.com>
2024-10-02 16:32:43 +0100

    Defined Syntax

--- Tests/Test.py
+++ Tests/Test.py
@@ -10,10 +10,38 @@ t.AddColumn(
     Age = int,
     Salary = float
 )
-t.RenameColumn("Name","FullName")
-t.FullName.Add("John","Doe","Jane","Doe","Alice","Wonderland")
-t.RemoveColumn("Age")
-print(t.Age)
+t.Insert(
+    Id=1,
+    Name="John",
+    Age=25,
+    Salary=50000.0
+)
+
+
+t.Select(
+    ["Name","Age"], # if all data is needed, pass Nothing Select(where=lambda row: row.Age > 20)
+    where=lambda row: row.Age > 20
+)
+
+t.Delete(0) #index
+t.Delete(where=lambda row: row.Age < 20) #condition
+
+t.Update(
+    0,  # index of the row
+    Name="Jane",  # fields to update
+    Age=30
+)
+
+t.Update(
+    where=lambda row: row.Age < 25,  # condition to find rows
+    Salary=55000.0  # fields to update
+)
+
+t.Update(
+    where=lambda row: row.Id == 1,  # condition to find rows
+    Salary=55000.0,  # fields to update
+    Age=60
+)
 
 
 #Column
